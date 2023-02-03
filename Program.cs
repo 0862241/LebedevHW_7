737ecdf3da07d6e6f540c807fa1e17858bdda262
0862241 <ptical@mail.ru>
2023-02-03 12:01:21 +0300

    Initial 7th Task2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// Task1. Specify a two-dimensional array of size m×n filled with random real numbers.
-
+/*
 Console.Write("Input a number of rows: ");
 int rows = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a number of columns: ");
@@ -36,24 +36,28 @@ void ShowArray(double[,] array)
     }
     Console.WriteLine();
 }
-
+*/
 
 // Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
-/*
-Console.Write("Enter the number of strings:  ");
-int linesVol = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the number of rows:  ");
+int rows = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter the number of columns: ");
-int columnsVol = Convert.ToInt32(Console.ReadLine());
-double[,] numbers = new double[linesVol, columnsVol];
-CrateArrayRandomNumbers(numbers);
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a minimum value: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a maximum value: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[rows, columns];
+CrateArrayRandom(numbers);
 ShowArray(numbers);
-void CrateArrayRandomNumbers(double[,] array)
+void CrateArrayRandom(double[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100));
+            array[i, j] = Convert.ToDouble(new Random().Next(minValue, maxValue));
         }
     }
 }
@@ -61,36 +65,36 @@ void ShowArray(double[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        Console.Write("[");
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write(array[i, j] + " ");
+            Console.Write(array[i, j] + "\t");
         }
-        Console.Write("]");
-        Console.WriteLine("");
+        Console.WriteLine();
+        
     }
+    Console.WriteLine();
 }
-Console.Write("Enter the item position number in the line: ");
-int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
-Console.Write("Enter the item position number in the column: ");
-int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
-if (pos1 < 0 || pos1 >= linesVol || pos2 < 0 || pos2 >= columnsVol)
+Console.Write("Input the position of number in the row: ");
+int posRow = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Input the position of number in the column: ");
+int posColumn = Convert.ToInt32(Console.ReadLine()) - 1;
+if (posRow < 0 || posRow >= rows || posColumn < 0 || posColumn >= columns)
 {
-    Console.WriteLine("There is no such element in the array.");
+    Console.WriteLine("There is no element with the specified indexes in this array");
 }
 else
 {
-    Console.WriteLine("Element value: " + numbers[pos1, pos2]);
+    Console.WriteLine("The requested array element is: " + numbers[posRow, posColumn]);
 }
-*/
+
 
 // Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 /*
 Console.Write("Enter the number of strings:  ");
-int linesVol  = Convert.ToInt32(Console.ReadLine());
+int rows  = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter the number of columns: ");
-int columnsVol = Convert.ToInt32(Console.ReadLine());
-double[,] numbers = new double[linesVol, columnsVol];
+int columns = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[rows, columns];
 CrateArrayRandomNumbers(numbers);
 ShowArray(numbers);
 
@@ -119,23 +123,23 @@ void ShowArray(double[,] array)
     }
 }
 
-int[,] array = new int[linesVol, columnsVol];
+int[,] array = new int[rows, columns];
 Random random = new Random();
-for (int i = 0; i < linesVol; i++)
+for (int i = 0; i < rows; i++)
 {
-    for (int j = 0; j < columnsVol; j++)
+    for (int j = 0; j < columns; j++)
     {
         array[i, j] = random.Next(0, 100);
     }
 }
-for (int j = 0; j < columnsVol; j++)
+for (int j = 0; j < columns; j++)
 {
     double sum = 0;
-    for (int i = 0; i < linesVol; i++)
+    for (int i = 0; i < rows; i++)
     {
         sum += array[i, j];
     }
-    double avg = sum / linesVol;
+    double avg = sum / rows;
     Console.WriteLine("Arithmetic mean of column elements " + (j + 1) + ": " + avg);
 }
 */
